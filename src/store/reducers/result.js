0b8a98e4d12a6f3428f4382204c742073a371548
Acostan13 <acostan@fau.edu>
2020-05-23 11:14:22 -0400

    leaning out switch cases

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,19 +1,22 @@
 import * as actionTypes from '../actions/actionTypes'
-import {updateObject} from '../utility'
+import { updateObject } from '../utility'
 
 const initialState = {
     results: []
 }
 
+const deleteResult = (state, action) => {
+    // Deleting elements immutably using filter()
+    const updatedArr = state.results.filter(result => result.id !== action.resultElId)
+    return updateObject(state, { results: updatedArr })
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            // Change data
-            return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.result})})
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) })
         case actionTypes.DELETE_RESULT:
-            // Deleting elements immutably using filter()
-            const updatedArr = state.results.filter(result => result.id !== action.resultElId)
-            return updateObject(state, {results: updatedArr})
+            return deleteResult(state, action)
         default:
     }
     return state
